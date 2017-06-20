0a6cd0124435b41f692d849cc542003d01c39a08
ctlente <ctlente@gmail.com>
2017-06-20 20:20:20 +0000

    Adicionei pesq_despesas

--- R/pesq.R
+++ R/pesq.R
@@ -95,3 +95,57 @@ pesq_deputados <- function(id_legislatura = NULL, sigla_uf = NULL,
   
   return(saida)
 }
+
+# Pesquisar despesas de um deputado
+pesq_despesas <- function(id_deputado, id_legislatura = NULL,
+                          ano = NULL, mes = NULL, cpf_cnpj = NULL,
+                          n_max = 15) {
+  
+  # Constantes da chamada
+  base <- stringr::str_c(
+    "https://dadosabertos.camara.leg.br/api/v2/deputados/",
+    id_deputado, "/despesas?")
+  fim <- "ordem=ASC&ordenarPor=numAno"
+  
+  # Criar listas de parâmetros
+  id_legislatura <- cria_param("idLegislatura", id_legislatura)
+  ano <- cria_param("ano", ano)
+  mes <- cria_param("mes", mes)
+  cpf_cnpj <- cria_param("cnpjCpfFornecedor", cpf_cnpj)
+  
+  # Unir todos os parâmetros da chamada
+  params <- stringr::str_c(
+    id_legislatura, ano, mes,
+    cpf_cnpj, fim, sep = "&")
+  
+  # Construir chamada
+  cham <- stringr::str_c(base, params)
+  
+  # Executar chamada
+  res <- jsonlite::fromJSON(cham)
+  
+  # Formatar resultados
+  saida <- tibble::as_tibble(res$dados)
+  names(saida) <- c("ano", "mes", "tipo_despesa",
+                    "id_documento", "tipo_documento",
+                    "data_documento", "num_documento",
+                    "valor_documento", "url_documento",
+                    "nome_fornecedor", "cpf_cnpj_fornecedor",
+                    "valor_liquido", "valor_glosa",
+                    "num_ressarcimento", "id_lote", "parcela")
+  
+  return(saida)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
